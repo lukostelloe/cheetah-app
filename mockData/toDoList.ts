c5fb6a6166d02dbbdd54ad7fd661cb5fbf2974bd
Abdiel Ramirez <abdiel.uveira@gmail.com>
2023-04-06 18:25:38 +0200

    feature(PatientModal): add patient to mockPatients local list

--- mockData/toDoList.ts
+++ mockData/toDoList.ts
@@ -1,4 +1,4 @@
-export const toDoList = [
+export const toDoList: string[] = [
   "create a to do list",
   "organise project into folders",
   "implement testing",
@@ -7,4 +7,5 @@ export const toDoList = [
   "create a text component",
   "change font globally",
   "create navigation sidebar",
+  "set add patient/add button to write in mockPatient list",
 ];
